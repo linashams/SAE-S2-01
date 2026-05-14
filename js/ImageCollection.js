3fcd18307638be470af26aa6bd2942f30aba1986
VAN Thuy Linh <thuylinhv1001@gmail.com>
2026-05-15 00:57:26 +0200

    update

---
+++ js/ImageCollection.js
@@ -0,0 +1,39 @@
+/**
+ * @type {ImagesCollection} imageCollections
+ */
+
+export const imageCollections = {
+    animals: [
+        {id: 1, name: "animal1", url: "./assets/images/animals/animal1.jpg"},
+        {id: 2, name: "animal2", url: "./assets/images/animals/animal2.jpg"},
+        {id: 3, name: "animal3", url: "./assets/images/animals/animal3.jpg"},
+        {id: 4, name: "animal4", url: "./assets/images/animals/animal4.jpg"},
+        {id: 5, name: "animal5", url: "./assets/images/animals/animal5.jpg"},
+        {id: 6, name: "animal6", url: "./assets/images/animals/animal6.jpg"},
+        {id: 7, name: "animal7", url: "./assets/images/animals/animal7.jpg"},
+        {id: 8, name: "animal8", url: "./assets/images/animals/animal8.jpg"},
+
+    ],
+    fruits: [
+        {id: 1, name: "fruit1", url: "./assets/images/fruits/fruit1.jpg"},
+        {id: 2, name: "fruit2", url: "./assets/images/fruits/fruit2.jpg"},
+        {id: 3, name: "fruit3", url: "./assets/images/fruits/fruit3.jpg"},
+        {id: 4, name: "fruit4", url: "./assets/images/fruits/fruit4.jpg"},
+        {id: 5, name: "fruit5", url: "./assets/images/fruits/fruit5.jpg"},
+        {id: 6, name: "fruit6", url: "./assets/images/fruits/fruit6.jpg"},
+        {id: 7, name: "fruit7", url: "./assets/images/fruits/fruit7.jpg"},
+        {id: 8, name: "fruit8", url: "./assets/images/fruits/fruit8.jpg"},
+
+    ],
+    cars: [
+        {id: 1, name: "car1", url: "./assets/images/cars/car1.jpg"},
+        {id: 2, name: "car2", url: "./assets/images/cars/car2.jpg"},
+        {id: 3, name: "car3", url: "./assets/images/cars/car3.jpg"},
+        {id: 4, name: "car4", url: "./assets/images/cars/car4.jpg"},
+        {id: 5, name: "car5", url: "./assets/images/cars/car5.jpg"},
+        {id: 6, name: "car6", url: "./assets/images/cars/car6.jpg"},
+        {id: 7, name: "car7", url: "./assets/images/cars/car7.jpg"},
+        {id: 8, name: "car8", url: "./assets/images/cars/car8.jpg"},
+
+    ],
+};
